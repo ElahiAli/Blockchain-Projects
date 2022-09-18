f18912bd7c5e428e7c15da452e69b4a8bc7ff713
ali elahi <alielahi090@gmail.com>
2022-09-18 09:04:26 +0430

    Third lesson

--- FundMe.sol
+++ FundMe.sol
@@ -52,11 +52,7 @@ contract FundMe {
     }
 
     //1000000000 => 1 Gwei
-    function getConversionRate(uint256 ethAmount)
-        public
-        view
-        returns (uint256)
-    {
+    function getConversionRate(uint256 ethAmount) public viewreturns(uint256) {
         uint256 ethPrice = getPrice();
         //devide because of editional 10 raised to 18 track on them. wei
         uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
