3dbfecbfbe222bda697db8fab656149b9a39281c
ali elahi <alielahi090@gmail.com>
2022-09-26 11:27:54 +0330

    Lesson Five

--- web3pySimpleStorage/demos/brownie_simple_storage/scripts/deploy.py
+++ web3pySimpleStorage/demos/brownie_simple_storage/scripts/deploy.py
@@ -1,4 +1,4 @@
-from brownie import accounts, config, SimpleStorage
+from brownie import accounts, config, SimpleStorage, network
 
 # import os
 
@@ -19,14 +19,21 @@ def deploy_simple_storage():
     # account = accounts.add(config["wallets"]["from_key"])
 
     # what happend in web3.py happening here.
-    account = accounts[0]
+    account = get_account()
     simple_storage = SimpleStorage.deploy({"from": account})
     stored_value = simple_storage.retrive()
     print(stored_value)
     transaction = simple_storage.store(15, {"from": account})
     transaction.wait(1)
-    update_store_value = transaction.retrive()
+    update_store_value = simple_storage.retrive()
     print(update_store_value)
+
+
+def get_account():
+    # network is keywork that allow us to intract with deffernt networks.
+    if network.show_active() == "development":
+        return accounts[0]
+    return accounts.add(config["wallets"]["from_key"])
 
 
 def main():
