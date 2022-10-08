5aacffd3b41a730107d8b86a31e3a9c8b2fec2f6
ElahiAli <ali_elahi00@outlook.com>
2022-10-08 20:06:12 +0330

    FundMe.sol-js

--- Blockchain_js/FundMe.sol
+++ Blockchain_js/FundMe.sol
@@ -1,12 +1,25 @@
-//SPDX-License-Identifier:MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "./PriceConverter.sol";
+
 contract FundMe{
+    using PriceConverter for uint256; 
+
+    uint256 public minimumUds = 50 * 1e18;
+    //list of address.
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
 
     function fund() public payable{
         //want to set a minimum amount to usd!
-        //
+        //getConversionRate(msg.value) it change after using our own library. 
+        require(msg.value.getConversionRate() > minimumUsd, "Didn't send enouph!");
+        //taking the eth sender account's address.
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
     }
 
+    
     // function withdraw(){}
 }
--- Blockchain_js/README.md
+++ Blockchain_js/README.md
@@ -8,3 +8,4 @@ store array with name and favorit number and returning the favorit number and ..
 storing transactions and show the address.<br/>
 using SimpleStorage functions and learning how to <strong>intract with another contract</strong>.<br/>
 we also used inheritance and override store fuction.<br/>
+learning how to build our own library and use it in our files.<br/>
