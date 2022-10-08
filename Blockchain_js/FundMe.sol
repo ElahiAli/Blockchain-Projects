5ae2f06f61d2818b3b023b6afe666a028261b7dc
ElahiAli <ali_elahi00@outlook.com>
2022-10-08 14:13:27 +0330

    fundme.sol

---
+++ Blockchain_js/FundMe.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
+
+contract FundMe{
+
+    function fund() public payable{
+        //want to set a minimum amount to usd!
+        //
+    }
+
+    // function withdraw(){}
+}
