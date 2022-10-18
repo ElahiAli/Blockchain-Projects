12c52a3697fa5a05e7c2a85044188f80c0824543
ElahiAli <ali_elahi00@outlook.com>
2022-10-18 14:07:27 +0330

    hardhat

---
+++ Blockchain_js/hh-fcc/hardhat-simple-storage/hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+};
+
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
