c178dbbc3ca27ad12184dd2a9c9a8eeb0e540904
ElahiAli <ali_elahi00@outlook.com>
2022-10-19 10:06:26 +0330

    interacting

--- Blockchain_js/hh-fcc/hardhat-simple-storage/scripts/deploy.js
+++ Blockchain_js/hh-fcc/hardhat-simple-storage/scripts/deploy.js
@@ -14,29 +14,48 @@ async function main() {
     console.log(`Deployed contract to: ${simpleStorage.address}`);
 
     //checking networks
-//     if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
-//         await simpleStorage.deployTransaction.wait(6);
-//         await verify(simpleStorage.address, []);
-//     }
-// }
+    if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+        await simpleStorage.deployTransaction.wait(6);
+        await verify(simpleStorage.address, []);
+    }
+
+    // interacting with retrieve and store function
+    const simpleStorageResponse = await simpleStorage.retrieve();
+    console.log(`Current value: ${simpleStorageResponse}`);
+    // uptading
+    const transactionResponse = await simpleStorage.store(9);
+    await transactionResponse.wait(1);
+    const updatedValue = await simpleStorage.retrieve();
+    console.log(`Updated Value is: ${updatedValue}`);
+
+    // interacting with addPerson function
+    const addPersonResponse = await simpleStorage.addPerson("hamed", "9");
+    await addPersonResponse.wait(1);
+    const peopleIndex = await simpleStorage.people(0);
+    console.log(`Name and FavoriteNumber : ${peopleIndex.toString()}`);
+
+    // interacting with mapping(nameToFavoriteNumber)
+    const nameTONumber = await simpleStorage.nameToFavoriteNumber("hamed");
+    console.log(`Favorite Number: ${nameTONumber}`);
+}
 
 // verifying contracts
-// async function verify(contractAddress, args) {
-//     console.log("Verifying contract...");
-//     // *second perameter* ; try used because of await.
-//     try {
-//         await run("verify:verify", {
-//             address: contractAddress,
-//             constructorArguments: args,
-//         });
-//     } catch (e) {
-//         if (e.message.toLowerCase().includes("already verified")) {
-//             console.log("Already Verified!");
-//         } else {
-//             console.log(e);
-//         }
-//     }
-// }
+async function verify(contractAddress, args) {
+    console.log("Verifying contract...");
+    // *second perameter* ; try used because of await.
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        });
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified!");
+        } else {
+            console.log(e);
+        }
+    }
+}
 
 main()
     .then(() => process.exit(0))
