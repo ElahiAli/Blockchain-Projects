7c237582776ccd93c143a73c947814a082fcf2f4
ElahiAli <ali_elahi00@outlook.com>
2022-11-01 07:56:31 +0330

    Raffle

--- Blockchain_js/hh-fcc/hardhat-smartcontract-lottery-fcc/contracts/Raffle.sol
+++ Blockchain_js/hh-fcc/hardhat-smartcontract-lottery-fcc/contracts/Raffle.sol
@@ -8,24 +8,28 @@
 
 pragma solidity ^0.8.8;
 
-error Lottety__NotEnoughETHEntered();
+error Raffle__NotEnoughETHEntered();
 
 contract Lottery {
     /*State Variable */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players; //it's payable because the winner get payed.
 
+    /* Events */
+    event RaffleEnter(address indexed players);
+
     constructor(uint256 enteranceFee) {
         i_entranceFee = enteranceFee;
     }
 
-    function enterLottery() public payable {
+    function enterRaffle() public payable {
         //check the entranceFee
         // require(msg.value > i_entranceFee,"not enough ETH")
         if (msg.value < i_entranceFee) {
-            revert Lottety__NotEnoughETHEntered();
+            revert Raffle__NotEnoughETHEntered();
         }
-        s_players.push(payable(msg.msg.sender)); //typecasted because it only accept payable address.
+        s_players.push(payable(msg.sender)); //typecasted because it only accept payable address.
+        emit RaffleEnter(msg.sender);
     }
 
     // function pickRandomWinner() {}
