cf518dab2ed9dd5eb1d6233222c3eeaa0b02d856
ElahiAli <ali_elahi00@outlook.com>
2023-03-08 15:34:44 +0330

    abi,binary,wallet, deploying...

--- Blockchain_js/hh-fcc/ethers-simple-storage/deploy.js
+++ Blockchain_js/hh-fcc/ethers-simple-storage/deploy.js
@@ -1,14 +1,24 @@
 const ethers = require("ethers");
-const fs = requir("fs");
-
+const fs = require("fs");
+// console.log(ethers.);
 async function main() {
   const provider = new ethers.providers.JsonRpcProvider(
-    "HTTP://127.0.0.1:7545"
+    "http://127.0.0.1:7545"
   );
   const wallet = new ethers.Wallet(
     "0x43bf92a843b0b35fc4fa3d3a175d211343b69e7f93ed0ec55ca55fb6d4d63651",
     provider
   );
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying, please wait...");
+  const contract = await contractFactory.deploy();
+  const transactionReceipt = await contract.deployTransaction.wait(1);
+  console.log(transactionReceipt);
 }
 
 main()
