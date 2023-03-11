323b8323ef06eea2ffa240f5dfa3c9e58704d0ed
ElahiAli <ali_elahi00@outlook.com>
2023-03-11 09:33:58 +0330

    network, env, hardhatetherscan

--- Blockchain_js/hh-fcc/hardhat-simple-storage/hardhat.config.js
+++ Blockchain_js/hh-fcc/hardhat-simple-storage/hardhat.config.js
@@ -1,14 +1,32 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
 module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+        },
+    },
     solidity: "0.8.8",
-};
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+}
 
 task("accounts", "Prints the list of accounts", async () => {
-    const accounts = await ethers.getSigners();
+    const accounts = await ethers.getSigners()
 
     for (const account of accounts) {
-        console.log(account.address);
+        console.log(account.address)
     }
-});
+})
