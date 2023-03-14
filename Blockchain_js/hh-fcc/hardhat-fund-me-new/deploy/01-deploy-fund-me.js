ed8875b7df2cec2f0211e3f3b3e5b42c76f59889
ElahiAli <ali_elahi00@outlook.com>
2023-03-14 11:06:03 +0330

    verify and deploy file

--- Blockchain_js/hh-fcc/hardhat-fund-me-new/deploy/01-deploy-fund-me.js
+++ Blockchain_js/hh-fcc/hardhat-fund-me-new/deploy/01-deploy-fund-me.js
@@ -1,17 +1,36 @@
 const { network } = require("hardhat");
+const { networkConfig, developmentChain } = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
 
-const { networkConfig } = require("../helper-hardhat-config");
-
-module.exports = async ({ getNameAccounts, deployments }) => {
+module.exports = async ({ getNamedAccounts, deployments }) => {
 	const { deploy, log } = deployments;
-	const { deployer } = getNameAccounts;
+	const { deployer } = await getNamedAccounts();
 	chainId = network.config.chainId;
 
-	const ethUsdPriceFeed = networkConfig[chainId]["ethUsdPriceFeed"];
+	let ethUsdPriceFeedAddress;
 
+	if (developmentChain.includes(network.name)) {
+		const ethUsdAggregator = await deployments.get("MockV3Aggregator");
+		ethUsdPriceFeedAddress = ethUsdAggregator.address;
+	} else {
+		ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+	}
+
+	const args = [ethUsdPriceFeedAddress];
 	const fundMe = await deploy("FundMe", {
 		from: deployer,
-		args: [],
+		args: args,
 		log: true,
+		waitConfirmations: network.config.blockConfirmations || 1,
 	});
+
+	if (
+		!developmentChain.includes(network.name) &&
+		process.env.ETHERSCAN_API_KEY
+	) {
+		await verify(fundMe.address, args);
+	}
+	log("--------------------------------------------------------------");
 };
+
+module.exports.tags = ["all", "fundme"];
