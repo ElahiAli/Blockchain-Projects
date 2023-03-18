ecca7ee1762a28b966d6ec01214c46638e360ba7
ElahiAli <ali_elahi00@outlook.com>
2023-03-18 14:53:25 +0330

    VRF2

--- Blockchain_js/hh-fcc/hardhat-smartcontract-lottery-new/contracts/Raffle.sol
+++ Blockchain_js/hh-fcc/hardhat-smartcontract-lottery-new/contracts/Raffle.sol
@@ -1,15 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
 error Raffle__NotEnoughEthEntered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
 
     event raffleEnter(address indexed sender);
 
-    constructor(uint256 entranceFee) {
+    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
     }
 
@@ -21,7 +24,12 @@ contract Raffle {
         emit raffleEnter(msg.sender);
     }
 
-    // function pickRandomWinner()  {}
+    function requestRandomWinner() external {}
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {}
 
     function getEnranceFee() public view returns (uint256) {
         return i_entranceFee;
