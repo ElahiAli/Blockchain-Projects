5947874dc035f5407c6368b0847fc841b5ef41c0
ElahiAli <ali_elahi00@outlook.com>
2023-03-21 12:51:27 +0330

    remove variables

--- Blockchain_js/hh-fcc/hardhat-smartcontract-lottery-new/helper-hardhat-config.js
+++ Blockchain_js/hh-fcc/hardhat-smartcontract-lottery-new/helper-hardhat-config.js
@@ -1,7 +1,7 @@
 const networkConfig = {
-    4: {
+    5: {
         name: "goerli",
-        vrfCoordinatorV2: "	0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
+        vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
         entranceFee: ethers.utils.parseEther("0.01"),
         gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
         subscriptionId: "0",
@@ -18,12 +18,8 @@ const networkConfig = {
 }
 
 const developmentChains = ["hardhat", "localhost"]
-const BASE_FEE = ethers.utils.parseEther("0.25")
-const GAS_PRICE_LINK = 1e9
 
 module.exports = {
     networkConfig,
     developmentChains,
-    BASE_FEE,
-    GAS_PRICE_LINK,
 }
