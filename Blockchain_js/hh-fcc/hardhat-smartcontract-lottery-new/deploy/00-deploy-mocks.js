c5838e9768558f530a00c3e73fa5084884e86320
ElahiAli <ali_elahi00@outlook.com>
2023-03-21 12:52:01 +0330

    fix bugs

--- Blockchain_js/hh-fcc/hardhat-smartcontract-lottery-new/deploy/00-deploy-mocks.js
+++ Blockchain_js/hh-fcc/hardhat-smartcontract-lottery-new/deploy/00-deploy-mocks.js
@@ -1,18 +1,19 @@
-const { deployments, getNamedAccounts, network, ethers } = require("hardhat")
-const { developmentChains, BASE_FEE, GAS_PRICE_LINK } = require("../helper-hardhat-config")
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
 
-module.exports = async function (deployments, getNamedAccounts) {
+const BASE_FEE = ethers.utils.parseEther("0.25")
+const GAS_PRICE_LINK = 1e9
+
+module.exports = async function ({ deployments, getNamedAccounts }) {
     const { deploy, log } = deployments
-    const deployer = (await getNamedAccounts()).deployer
-    const args = [BASE_FEE, GAS_PRICE_LINK]
+    const { deployer } = await getNamedAccounts()
 
     if (developmentChains.includes(network.name)) {
         log("Local network detected! Dploying Mock...")
         await deploy("VRFCoordinatorV2Mock", {
-            contract: "VRFCoordinatorV2Mock",
             from: deployer,
             log: true,
-            args: args,
+            args: [BASE_FEE, GAS_PRICE_LINK],
         })
         log("Mock deployed!")
         log("------------------------------------------------------------")
