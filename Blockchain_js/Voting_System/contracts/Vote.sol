0fa698c2d92aa885d71f2eb3e71db6f556712d07
ElahiAli <ali_elahi00@outlook.com>
2023-03-30 14:46:51 +0330

    contracts completed.

--- Blockchain_js/Voting_System/contracts/Vote.sol
+++ Blockchain_js/Voting_System/contracts/Vote.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.8;
+pragma solidity ^0.8.7;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 error Vote__NoteRegistered();
+error Vote__FoodNotExists();
+error Vote__AccessDenied();
+error Vote__AlreadyVoted();
 
 contract Vote is Ownable {
     uint256 public constant MINIMUM_AGE_OF_VOTERS = 9;
@@ -12,6 +15,7 @@ contract Vote is Ownable {
         string name;
         uint256 age;
         string election;
+        bool vote;
     }
 
     //different options for vote
@@ -19,15 +23,33 @@ contract Vote is Ownable {
     Participants[] internal registrationList;
     string[] internal foodElectionList;
 
-    mapping(string => address) public nameToAddress;
-    mapping(string => mapping(string => uint256)) public foodIndexTOFoodVote;
-    mapping(string => uint256) public statusToCount;
+    mapping(string => address) internal nameToAddress;
+    mapping(string => mapping(string => uint256)) internal foodIndexToFoodVote;
+    mapping(address => uint256) internal addressToIndex;
 
     modifier ageLimit(uint256 _age) {
         require(
-            _age <= MINIMUM_AGE_OF_VOTERS,
+            _age >= MINIMUM_AGE_OF_VOTERS,
             "your age is not enough for votign."
         );
+        _;
+    }
+
+    modifier checkFoodExistent(string memory _election) {
+        string[] memory newfoodElectionList = getElectionsList();
+        bool found = false;
+        for (uint256 index = 0; index < newfoodElectionList.length; index++) {
+            if (
+                keccak256(abi.encodePacked(_election)) ==
+                keccak256(abi.encodePacked(newfoodElectionList[index]))
+            ) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            revert Vote__FoodNotExists();
+        }
         _;
     }
 
@@ -36,32 +58,62 @@ contract Vote is Ownable {
         string memory _name,
         uint256 _age,
         string memory _election
-    ) public ageLimit(_age) {
-        registrationList.push(Participants(_name, _age, _election));
+    ) public ageLimit(_age) checkFoodExistent(_election) {
+        registrationList.push(Participants(_name, _age, _election, false));
         nameToAddress[_name] = msg.sender;
+        addressToIndex[msg.sender] = (registrationList.length) - 1;
     }
 
     //Election list by Owner
-    function electionList(string memory _foodName) public onlyOwner {
-        foodElectionList.push(_foodName);
+    function electionList(string[] calldata _foodName) public onlyOwner {
+        for (uint256 i = 0; i < _foodName.length; i++) {
+            foodElectionList.push(_foodName[i]);
+        }
     }
 
-    //voting anonymously
-    //Vote counting
+    //Vote counting && voting anonymously
     function vote(
         string memory _name,
         uint256 _foodIndex,
         uint256 _statusIndex
     ) public {
+        if (
+            keccak256(
+                abi.encodePacked(
+                    registrationList[addressToIndex[msg.sender]].election
+                )
+            ) != keccak256(abi.encodePacked(foodElectionList[_foodIndex]))
+        ) {
+            revert Vote__AccessDenied();
+        }
+        if (registrationList[addressToIndex[msg.sender]].vote == true) {
+            revert Vote__AlreadyVoted();
+        } else {
+            registrationList[addressToIndex[msg.sender]].vote = true;
+        }
+
+        string[] memory electionsList = getElectionsList();
+        string[] memory options = getOptionsList();
         if (nameToAddress[_name] != msg.sender) {
             revert Vote__NoteRegistered();
         }
-        uint256 count = statusToCount[_statusIndex];
-        statusToCount[_statusIndex] = count + 1;
-        foodIndexTOFoodVote[foodElectionList[_foodIndex]] = statusToCount;
+        foodIndexToFoodVote[electionsList[_foodIndex]][options[_statusIndex]]++;
     }
 
     //Results publishing
+    function electionResult(
+        string memory _foodName
+    )
+        public
+        view
+        returns (uint256 Bad, uint256 Notbad, uint256 Good, uint256 Awesome)
+    {
+        uint256 badVots = foodIndexToFoodVote[_foodName]["Bad"];
+        uint256 notbadVots = foodIndexToFoodVote[_foodName]["Notbad"];
+        uint256 GoodVots = foodIndexToFoodVote[_foodName]["Good"];
+        uint256 awesomeVots = foodIndexToFoodVote[_foodName]["Awesome"];
+        return (badVots, notbadVots, GoodVots, awesomeVots);
+    }
 
     // showing different elections
     function getElectionsList() public view returns (string[] memory) {
@@ -71,5 +123,10 @@ contract Vote is Ownable {
     // showing different options
     function getOptionsList() public view returns (string[] memory) {
         return Options;
+    }
+
+    //getting participantes
+    function getParticipants() public view returns (Participants[] memory) {
+        return registrationList;
     }
 }
