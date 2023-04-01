81bce2f732249478e73455c32a8aab257f8c0cc7
ElahiAli <ali_elahi00@outlook.com>
2023-04-01 14:58:34 +0330

    initial unit test

---
+++ Blockchain_js/Voting_System/test/unit/vote.test.js
@@ -0,0 +1,10 @@
+const { network } = require("hardhat");
+
+// const {}
+developmentChains[("hardhat", "localhost")];
+
+!developmentChains.includes(network.name)
+	? describe.skipe
+	: describe("Vote Unit Test", function () {
+			beforeEach();
+	  });
