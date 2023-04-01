12e830f4abdd219d2f304e24f8587d9e3799e71c
ElahiAli <ali_elahi00@outlook.com>
2023-04-01 12:16:27 +0330

    network and accounts detail

--- Blockchain_js/Voting_System/hardhat.config.js
+++ Blockchain_js/Voting_System/hardhat.config.js
@@ -3,5 +3,19 @@ require("hardhat-deploy");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+	solidity: "0.8.18",
+	defaultNetwork: "hardhat",
+	networks: {
+		localhost: {
+			url: "http://127.0.0.1:8545/",
+			// accounts: thanks hardhat!
+			chainId: 31337,
+		},
+	},
+	namedAccounts: {
+		deployer: {
+			default: 0, // here this will by default take the first account as deployer
+			1: 0,
+		},
+	},
 };
