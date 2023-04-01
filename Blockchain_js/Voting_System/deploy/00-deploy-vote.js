803a4a0fc2a0e92ff40e8c32cfe23116b89bec6c
ElahiAli <ali_elahi00@outlook.com>
2023-04-01 12:17:22 +0330

    deploying

---
+++ Blockchain_js/Voting_System/deploy/00-deploy-vote.js
@@ -0,0 +1,22 @@
+const { network } = require("hardhat");
+
+developmentChains = ["hardhat", "localhost"];
+
+module.exports = async function ({ deployments, getNamedAccounts }) {
+	const { deploy, log } = deployments;
+	const { deployer } = await getNamedAccounts();
+
+	log("Deploying...");
+	const vote = await deploy("Vote", {
+		from: deployer,
+		log: true,
+		args: [],
+	});
+
+	if (!developmentChains.includes(network.name)) {
+		log("Verifying...");
+		await verify(vote.address, []);
+	}
+};
+
+module.exports.tags = ["all", "vote"];
---
+++ Blockchain_js/Voting_System/scripts/vote.js
@@ -0,0 +1,39 @@
+const { getNamedAccounts, ethers } = require("hardhat");
+
+async function main() {
+	const { deployer } = await getNamedAccounts();
+	const vote = await ethers.getContract("Vote", deployer);
+
+	console.log("entering food list");
+	const transactionElectionList = await vote.electionList([
+		"Hamburger",
+		"Cheeseburger",
+		"Sandwich",
+	]);
+	await transactionElectionList.wait(1);
+
+	console.log("Registering...");
+	const transactionRegister = await vote.registration("ali", 22, "Hamburger");
+	await transactionRegister.wait(1);
+
+	console.log("Voting...");
+	const transactionResponse = await vote.vote("ali", 0, 3);
+	await transactionResponse.wait(1);
+	console.log("Voted!");
+
+	console.log("result: ");
+	const transactionResult = await vote.electionResult("Hamburger");
+	console.log(
+		`Bad= ${transactionResult.toString()[0]}`,
+		`NotBad= ${transactionResult.toString()[2]}`,
+		`Good= ${transactionResult.toString()[4]}`,
+		`Awesome= ${transactionResult.toString()[6]}`
+	);
+}
+
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.log(error);
+		process.exit(1);
+	});
