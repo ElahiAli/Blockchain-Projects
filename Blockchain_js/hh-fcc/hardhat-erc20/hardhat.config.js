d209c3448166e77df8c6edc7f5d449f6c384e1d5
ElahiAli <ali_elahi00@outlook.com>
2023-04-03 14:37:12 +0330

    hardhat initialized

---
+++ Blockchain_js/hh-fcc/hardhat-erc20/hardhat.config.js
@@ -0,0 +1,41 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("solidity-coverage");
+require("hardhat-gas-reporter");
+require("hardhat-contract-sizer");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    localhost: {
+      chainId: 31337,
+    },
+  },
+  gasReporter: {
+    enabled: false,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
+  contractSizer: {
+    runOnCompile: false,
+    only: ["Raffle"],
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+    player: {
+      default: 1,
+    },
+  },
+};
---
+++ Blockchain_js/hh-fcc/hardhat-erc20/helper-hardhat-config.js
@@ -0,0 +1,18 @@
+const networkConfig = {
+  31337: {
+    name: "localhost",
+  },
+};
+
+const developmentChains = ["hardhat", "localhost"];
+const INITIAL_SUPPLY = "1000000000000000000000000";
+const TOKEN_NAME = "ManualToken";
+const TOKEN_SYMBOL = "OT";
+
+module.exports = {
+  //   networkConfig,
+  developmentChains,
+  INITIAL_SUPPLY,
+  TOKEN_NAME,
+  TOKEN_SYMBOL,
+};
