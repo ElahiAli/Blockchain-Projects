8b507a730f877700d04e19d6f8d764e6258d712a
ElahiAli <ali_elahi00@outlook.com>
2023-04-04 15:52:30 +0330

    updated

--- Blockchain_js/hh-fcc/hardhat-erc20/hardhat.config.js
+++ Blockchain_js/hh-fcc/hardhat-erc20/hardhat.config.js
@@ -8,33 +8,27 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.7",
   defaultNetwork: "hardhat",
   networks: {
-    hardhat: {
-      chainId: 31337,
-    },
     localhost: {
+      url: "http://127.0.0.1:8545/",
       chainId: 31337,
     },
   },
   gasReporter: {
     enabled: false,
-    currency: "USD",
     outputFile: "gas-report.txt",
     noColors: true,
-    // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
-  },
-  contractSizer: {
-    runOnCompile: false,
-    only: ["Raffle"],
+    currency: "USD",
+    // coinmarketcap: COINMARKETCAP_API_KEY, //get file for ETH to USD
+    // token: "ETH", //polygon
   },
   namedAccounts: {
     deployer: {
-      default: 0, // here this will by default take the first account as deployer
-      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+      default: 0,
     },
-    player: {
+    client: {
       default: 1,
     },
   },
