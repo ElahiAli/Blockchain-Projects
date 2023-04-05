741bdb8c416544ee53a79c79112a236ce37edb56
ElahiAli <ali_elahi00@outlook.com>
2023-04-05 12:26:55 +0330

    intract with deposit function

---
+++ Blockchain_js/hh-fcc/hardhat-defi/scripts/getWeth.js
@@ -0,0 +1,20 @@
+const { getNamedAccounts, ethers } = require("hardhat");
+const Amount = ethers.utils.parseEther("0.02");
+
+async function getWeth() {
+    const { deployer } = await getNamedAccounts();
+    // call the deposit function on the weth contract : 1.Abi 2.Address
+
+    const iWeth = await ethers.getContractAt(
+        "IWeth",
+        "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+        deployer
+    );
+
+    const tx = await iWeth.deposit({ value: Amount });
+    await tx.wait(1);
+    const wethBalance = await iWeth.balanceOf(deployer);
+    console.log(`Got ${wethBalance.toString()} Weth.`);
+}
+
+module.exports = { getWeth };
