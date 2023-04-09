d0f83ebe75ce7181507b409ec9947848b00cd706
ElahiAli <ali_elahi00@outlook.com>
2023-04-09 14:54:39 +0330

    update packages and network

--- Blockchain_js/hh-fcc/hardhat-nft/helper-hardhat-config.js
+++ Blockchain_js/hh-fcc/hardhat-nft/helper-hardhat-config.js
@@ -1,21 +1,22 @@
 const networkConfig = {
-    31337: {
-        name: "localhost",
-        ethUsdPriceFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
-        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei
-        mintFee: "10000000000000000", // 0.01 ETH
-        callbackGasLimit: "500000", // 500,000 gas
-    },
-    // Price Feed Address, values can be obtained at https://docs.chain.link/data-feeds/price-feeds/addresses
-    11155111: {
-        name: "sepolia",
-        ethUsdPriceFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
-        vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
-        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
-        callbackGasLimit: "500000", // 500,000 gas
-        mintFee: "10000000000000000", // 0.01 ETH
-        subscriptionId: "1002", // add your ID here!
-    },
+  5: {
+    name: "goerli",
+    vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D", //https://docs.chain.link/docs/vrf/v2/subscription/supported-networks/
+    entranceFee: ethers.utils.parseEther("0.01"),
+    gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
+    subscriptionId: "0",
+    callbackGasLimit: "500000",
+    mintFee: "10000000000000000", // 0.01 ETH
+    interval: "30",
+  },
+  31337: {
+    name: "hardhat",
+    entranceFee: ethers.utils.parseEther("0.01"),
+    gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", //doesn't matter
+    callbackGasLimit: "500000",
+    mintFee: "10000000000000000", // 0.01 ETH
+    interval: "30",
+  },
 };
 
 const DECIMALS = "18";
@@ -23,8 +24,8 @@ const INITIAL_PRICE = "200000000000000000000";
 const developmentChains = ["hardhat", "localhost"];
 
 module.exports = {
-    networkConfig,
-    developmentChains,
-    DECIMALS,
-    INITIAL_PRICE,
+  networkConfig,
+  developmentChains,
+  DECIMALS,
+  INITIAL_PRICE,
 };
