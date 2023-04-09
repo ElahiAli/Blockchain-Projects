06e6345465080e6e6075b2b402bb67adb205e5dd
ElahiAli <ali_elahi00@outlook.com>
2023-04-09 14:56:15 +0330

    change the tags

--- Blockchain_js/hh-fcc/hardhat-nft/deploy/01-deploy-basic-nft.js
+++ Blockchain_js/hh-fcc/hardhat-nft/deploy/01-deploy-basic-nft.js
@@ -3,24 +3,24 @@ const { developmentChains } = require("../helper-hardhat-config");
 const { verify } = require("../utils/verify");
 
 module.exports = async function ({ deployments, getNamedAccounts }) {
-    const { deploy, log } = deployments;
-    const { deployer } = await getNamedAccounts();
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
 
-    log("-------------------------------------------------");
-    const args = [];
+  log("-------------------------------------------------");
+  const args = [];
 
-    const basicNFT = await deploy("basicNFT", {
-        from: deployer,
-        log: true,
-        args: args,
-    });
+  const basicNFT = await deploy("basicNFT", {
+    from: deployer,
+    log: true,
+    args: args,
+  });
 
-    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
-        log("Verifying...");
-        await verify(basicNFT.address, args);
-    }
-    log("Deployed.");
-    log("-----------------------------------------------------------");
+  if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+    log("Verifying...");
+    await verify(basicNFT.address, args);
+  }
+  log("Deployed.");
+  log("-----------------------------------------------------------");
 };
 
-module.exports.tags = ["all", "main"];
+module.exports.tags = ["all", "basicNft", "main"];
