ed5fe2501b76c84d92213e8ffe4bf41b4b3dee84
ElahiAli <ali_elahi00@outlook.com>
2023-04-12 17:17:19 +0330

    updated

--- Blockchain_js/hh-fcc/hardhat-nft/helper-hardhat-config.js
+++ Blockchain_js/hh-fcc/hardhat-nft/helper-hardhat-config.js
@@ -5,7 +5,7 @@ const networkConfig = {
     gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
     subscriptionId: "0",
     callbackGasLimit: "500000",
-    mintFee: "100000000", // 0.01 ETH
+    mintFee: "10000000000000000", // 0.01 ETH
   },
 
   31337: {
