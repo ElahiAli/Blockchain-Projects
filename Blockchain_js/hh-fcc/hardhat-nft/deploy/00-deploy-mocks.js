1e945d31b5b6c714e065b23a0259bdec11a33ed1
ElahiAli <ali_elahi00@outlook.com>
2023-04-16 09:53:46 +0330

    adding mockV3Aggregator

--- Blockchain_js/hh-fcc/hardhat-nft/deploy/00-deploy-mocks.js
+++ Blockchain_js/hh-fcc/hardhat-nft/deploy/00-deploy-mocks.js
@@ -4,6 +4,9 @@ const { developmentChains } = require("../helper-hardhat-config");
 const BASE_FEE = ethers.utils.parseEther("0.25");
 const GAS_PRICE_LINK = 1e9;
 
+const DECIMALS = "18";
+const INITIAL_PRICE = ethers.utils.parseUnits("2000", "ether");
+
 module.exports = async function ({ deployments, getNamedAccounts }) {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
@@ -14,6 +17,12 @@ module.exports = async function ({ deployments, getNamedAccounts }) {
       from: deployer,
       log: true,
       args: [BASE_FEE, GAS_PRICE_LINK],
+    });
+
+    await deploy("MockV3Aggregator", {
+      from: deployer,
+      log: true,
+      args: [DECIMALS, INITIAL_PRICE],
     });
   }
   log("Mock Deployed.");
