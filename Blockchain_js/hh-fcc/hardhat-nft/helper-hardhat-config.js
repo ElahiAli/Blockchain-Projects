3fc1cc2fbc48e2633bdff68dcd3b689bef2d28f2
ElahiAli <ali_elahi00@outlook.com>
2023-04-16 09:52:42 +0330

    add ethUsdPriceFeed

--- Blockchain_js/hh-fcc/hardhat-nft/helper-hardhat-config.js
+++ Blockchain_js/hh-fcc/hardhat-nft/helper-hardhat-config.js
@@ -6,11 +6,12 @@ const networkConfig = {
     subscriptionId: "0",
     callbackGasLimit: "500000",
     mintFee: "10000000000000000", // 0.01 ETH
+    ethUsdPriceFeed: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
   },
 
   31337: {
     name: "localhost",
-    ethUsdPriceFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
+    // ethUsdPriceFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
     gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 30 gwei
     mintFee: "10000000000000000", // 0.01 ETH
     callbackGasLimit: "500", // 500,000 gas
