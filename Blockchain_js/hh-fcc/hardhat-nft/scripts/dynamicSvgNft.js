1ac4a9d08a8119452e4ec29dcafddbaa860d3202
ElahiAli <ali_elahi00@outlook.com>
2023-04-17 11:56:03 +0330

    DynamicNft: interacting with mintNft function

--- Blockchain_js/hh-fcc/hardhat-nft/scripts/dynamicSvgNft.js
+++ Blockchain_js/hh-fcc/hardhat-nft/scripts/dynamicSvgNft.js
@@ -2,23 +2,17 @@ const { getNamedAccounts, ethers, deployments } = require("hardhat");
 
 // getting token URIs
 async function main() {
-  await deployments.fixture(["all"]);
+  await deployments.fixture(["dynamicSvg", "mock"]);
   const { deployer } = await getNamedAccounts();
+
+  console.log("Getting deployed contract...");
   const dynamicNft = await ethers.getContract("dynamicNft", deployer);
-  const mockV3Aggregator = await ethers.getContract("MockV3Aggregator", deployer);
-
-  // console.log(mockV3Aggregator.address);
-
-  const lowSvg = await dynamicNft.getLowSvg();
-  const highSvg = await dynamicNft.getHighSvg();
-
-  const highValue = await ethers.utils.parseEther("1000000000000");
-  await dynamicNft.mintNft(highValue);
+  console.log("interacting with mintNft function...");
+  const highValue = ethers.utils.parseEther("4000");
+  const dynamicNftMintTx = await dynamicNft.mintNft(highValue.toString());
+  await dynamicNftMintTx.wait(1);
   const tokenUri = await dynamicNft.tokenURI(0);
-
-  console.log(tokenUri);
-  // console.log("lowSvg: ", lowSvg);
-  // console.log("highSvg: ", highSvg);
+  console.log(`Dynamic NFT has tokenURI: ${await tokenUri}`);
 }
 
 main()
