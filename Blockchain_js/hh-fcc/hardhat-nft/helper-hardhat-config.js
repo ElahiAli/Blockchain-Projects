f50926d8fc16202ea065cd5a6a4d64fbc9d3d1b0
ElahiAli <ali_elahi00@outlook.com>
2023-04-17 11:54:38 +0330

    add sunbscription id

--- Blockchain_js/hh-fcc/hardhat-nft/helper-hardhat-config.js
+++ Blockchain_js/hh-fcc/hardhat-nft/helper-hardhat-config.js
@@ -3,7 +3,7 @@ const networkConfig = {
     name: "sepolia",
     vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625", //https://docs.chain.link/docs/vrf/v2/subscription/supported-networks/
     gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
-    subscriptionId: "0",
+    subscriptionId: "1242",
     callbackGasLimit: "500000",
     mintFee: "10000000000000000", // 0.01 ETH
     ethUsdPriceFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
