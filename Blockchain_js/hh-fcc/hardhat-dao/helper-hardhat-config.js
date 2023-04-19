caf38ba0dd532e79b27ebfc73855e0a4e2aa2ab8
ElahiAli <ali_elahi00@outlook.com>
2023-04-19 15:00:58 +0330

    add new variable

--- Blockchain_js/hh-fcc/hardhat-dao/helper-hardhat-config.js
+++ Blockchain_js/hh-fcc/hardhat-dao/helper-hardhat-config.js
@@ -2,8 +2,7 @@ const networkConfig = {
   11155111: {
     name: "sepolia",
     vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625", //https://docs.chain.link/docs/vrf/v2/subscription/supported-networks/
-    gasLane:
-      "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
+    gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
     subscriptionId: "1242",
     callbackGasLimit: "500000",
     mintFee: "10000000000000000", // 0.01 ETH
@@ -13,20 +12,25 @@ const networkConfig = {
   31337: {
     name: "localhost",
     // ethUsdPriceFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
-    gasLane:
-      "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 30 gwei
+    gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 30 gwei
     mintFee: "10000000000000000", // 0.01 ETH
     callbackGasLimit: "500", // 500,000 gas
   },
 };
 
-const DECIMALS = "18";
-const INITIAL_PRICE = "200000000000000000000";
+const MIN_DELAY = 3600;
+const VOTING_PERIOD = 5;
+const VOTING_DELAY = 1;
+const QUORUM_PERCENTAGE = 4;
 const developmentChains = ["hardhat", "localhost"];
+const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
 
 module.exports = {
   networkConfig,
   developmentChains,
-  DECIMALS,
-  INITIAL_PRICE,
+  MIN_DELAY,
+  VOTING_PERIOD,
+  VOTING_DELAY,
+  QUORUM_PERCENTAGE,
+  ADDRESS_ZERO,
 };
