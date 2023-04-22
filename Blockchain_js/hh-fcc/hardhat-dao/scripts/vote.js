b5d826a33c0c1a06e4d391fc77793435f6825b3d
ElahiAli <ali_elahi00@outlook.com>
2023-04-22 08:45:19 +0330

    vote script

--- Blockchain_js/hh-fcc/hardhat-dao/scripts/vote.js
+++ Blockchain_js/hh-fcc/hardhat-dao/scripts/vote.js
@@ -1,5 +1,10 @@
 const fs = require("fs");
-const { PROPOSAL_FILE, developmentChains, VOTING_DELAY } = require("../helper-hardhat-config");
+const {
+  PROPOSAL_FILE,
+  developmentChains,
+  VOTING_DELAY,
+  VOTING_PERIOD,
+} = require("../helper-hardhat-config");
 const { network, ethers } = require("hardhat");
 const { moveBlock } = require("../utils/move-block");
 
@@ -9,21 +14,23 @@ async function main(proposalIndex) {
   const proposal = JSON.parse(fs.readFileSync(PROPOSAL_FILE, "utf8"));
   const proposalId = proposal[network.config.chainId][proposalIndex];
 
+  if (developmentChains.includes(network.name)) {
+    await moveBlock(VOTING_DELAY + 1);
+  }
+
   // 0=Against, 1=For, 2=Abstain
   const voteWay = 1;
   const governor = await ethers.getContract("GovernorContract");
   const reason = "I like a do da cha cha.";
   const voteTxResponse = await governor.castVoteWithReason(proposalId, voteWay, reason);
   await voteTxResponse.wait(1);
+  console.log(`Proposal Statte: ${await governor.state(proposalId)}`);
 
   if (developmentChains.includes(network.name)) {
-    await moveBlock(VOTING_DELAY + 1);
+    await moveBlock(VOTING_PERIOD + 1);
   }
-  console.log("Voted! Ready to GO.");
-  console.log(await governor.state(proposalId.toString()));
-  if ((await governor.state(proposalId.toString())) === "4") {
-    console.log("Proposal secceeded!");
-  }
+  console.log("Voting is over.");
+  console.log(`Proposal State is: ${await governor.state(proposalId)}`);
 }
 
 main(index)
