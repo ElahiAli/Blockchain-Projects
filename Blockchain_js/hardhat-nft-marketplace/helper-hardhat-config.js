08030dae935cdec602b229079f8365c1ceaf5a46
ElahiAli <ali_elahi00@outlook.com>
2023-04-29 12:08:03 +0330

    init hardhat

---
+++ Blockchain_js/hardhat-nft-marketplace/helper-hardhat-config.js
@@ -0,0 +1,27 @@
+const { ethers } = require("hardhat");
+
+const networkConfig = {
+    5: {
+        name: "goerli",
+        vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D", //https://docs.chain.link/docs/vrf/v2/subscription/supported-networks/
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
+        subscriptionId: "0",
+        callbackGasLimit: "500000",
+        interval: "30",
+    },
+    31337: {
+        name: "hardhat",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", //doesn't matter
+        callbackGasLimit: "500000",
+        interval: "30",
+    },
+};
+
+const developmentChains = ["hardhat", "localhost"];
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+};
